d5bd671060ed16666376db29ad030645c8702bb2
Nez Mustafa <nezmustafa123@gmail.com>
2021-12-24 16:00:10 +0000

    input style changes

--- src/js/config.js
+++ src/js/config.js
@@ -7,3 +7,4 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const API_KEY = "298bdcad-74ed-46bf-9cc9-8197606d68fa";
+export const MODAL_CLOSE_SEC = 2.5;
